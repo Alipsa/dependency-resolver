af168dd21569a8e3dbc63c08127beaa03ae33670
per <per@alipsa.se>
2026-02-10 20:56:56 +0100

    release 1.0.2

--- release.sh
+++ release.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
-source ~/.sdkman/bin/sdkman-init.sh
-source jdk17
+set -e
+
+if [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]]; then
+  echo "Initializing SDKMAN environment..."
+  source "$HOME/.sdkman/bin/sdkman-init.sh"
+fi
+
+if command -v jdk17 >/dev/null 2>&1; then
+  . jdk17
+fi
+
+java_version_line="$(java -version 2>&1 | head -n1)"
+if [[ $java_version_line =~ \"([0-9]+)(\.([0-9]+))? ]]; then
+  java_major="${BASH_REMATCH[1]}"
+  if [[ "$java_major" == "1" ]]; then
+    java_major="${BASH_REMATCH[3]}"
+  fi
+fi
+
+if [[ "$java_major" != "17" ]]; then
+  echo "Error: Java 17 is required. Found: $java_version_line in $(which java)}" >&2
+  exit 1
+fi
+
 ./gradlew clean release
